8794ce741dc37f6818180d75b0e036c76fa16483
Jake Johnson <kognizant.kog@gmail.com>
2017-08-19 15:16:07 -0400

    some comments

--- app/controllers/FlightsController.scala
+++ app/controllers/FlightsController.scala
@@ -14,6 +14,8 @@ package controllers {
 
   @Singleton
   class FlightsController @Inject() (cc: ControllerComponents, ws: WSClient) extends AbstractController(cc) {
+    // only returning json resp right now; for testing deduping algorithm;
+    //   frontend has not been created yet
     def index = Action.async {
       val aggFlightsURI = "https://private-7e668-flightaggregationapi.apiary-mock.com/flights"
       val singleProvFlightsURI = "https://private-6516c-singleproviderapi.apiary-mock.com/flights"
@@ -23,6 +25,7 @@ package controllers {
       }
     }
 
+    // this is just here temporarily; should be moved to some sort of helper module
     def getAggregatedFlights(request : WSRequest) : Future[String] = {
       implicit val system : ActorSystem = ActorSystem()
       request.get().map { resp => resp.body }(system.dispatcher)
@@ -37,6 +40,9 @@ package controllers {
     }
 
     def dedupedFlights(req1: WSRequest, req2: WSRequest) : Future[String] = {
+      // I might add comments here in real life--probably if whoever reviewed
+      //   the code found it hard to follow. However, if you look at the
+      //   raw response, it would be faily easy to follow.
       for {
         singleProvFlightsJson <- getSingleProvFlights(req1)
         allAggFlightsJson <- getAggregatedFlights(req2)
@@ -56,12 +62,17 @@ package controllers {
   }
 }
 
+// this is just here temporarily; would be its own file
 package jakelib {
   import com.fasterxml.jackson.databind.ObjectMapper
   import com.fasterxml.jackson.module.scala.experimental.ScalaObjectMapper
   import com.fasterxml.jackson.module.scala.DefaultScalaModule
 
-  // This is fairly naive, but it makes my life easier here
+  // This is fairly naive, but it makes my life easier here.
+  //   It could be useful if polished and not reliant on jackson.
+  //   I need a #fromJson method so that I can build a map out of
+  //   json string pared by #toJson, as jackson cannot digest a
+  //   string with escape sequences.
   object JakesSanerJsonService {
     val mapper = new ObjectMapper() with ScalaObjectMapper
     mapper.registerModule(DefaultScalaModule)
@@ -74,6 +85,8 @@ package jakelib {
       toJson(value.asInstanceOf[Map[String, Any]])
     }
 
+    // Need #fromJson method that strips indigestible parts of string:
+    //   mapper.readValue[Map[String, T]](fromJson(json))
     def toMap[T](json : String)(implicit m : Manifest[T]) = {
       mapper.readValue[Map[String, T]](json)
     }
